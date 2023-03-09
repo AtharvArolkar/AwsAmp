8ec68be669511136fec6c1470b5645f28cd23945
atharv_arolkar <atharv_arolkar@persistent.com>
2023-03-09 11:49:49 +0530

    initial commit

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,41 @@
+import { FileUploader, Theme, ThemeProvider, View } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import './Amp.css';
+
+export const DefaultFileUploaderExample = () => {
+  
+  const theme :Theme = {
+    name: "my-theme",
+    tokens: {
+      colors: {},
+      borderWidths: {
+        small: { value: "2px" },
+        medium: { value: "4px" },
+        large: { value: "8px" },
+      },
+      radii: {
+        xs: { value: "1rem" },
+        small: { value: "2rem" },
+        medium: { value: "2rem" },
+        large: { value: "2rem" },
+        xl: { value: "3rem" },
+      },
+    },
+  };
+  return (
+    <div className="browse-file">
+      <ThemeProvider theme={theme}>
+        <View>
+          <FileUploader
+            // variation='primary'            
+            hasMultipleFiles={true}
+            acceptedFileTypes={["image/*", "video/*"]}
+            accessLevel="public"
+            // onChange={}
+            
+          />
+        </View>
+      </ThemeProvider>
+    </div>
+  );
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import { DefaultFileUploaderExample } from './FileUpload';
+import FileView from './FileView';
+
+export default function Navbar(){
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">AWS Amplify</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link active"  to="/">Upload</Link>
+        </li>
+        
+        <li className="nav-item">
+          <Link className="nav-link active"  to="/view">Fetch</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  
+</nav>
+    )
+}
